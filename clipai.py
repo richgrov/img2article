e02559c6037a2697ae9e06bccbd53126f8103fe0
MasterDash5 <constant4337@molecularmail.com>
2024-11-20 14:39:23 -0700

    delete something

--- clipai.py
+++
@@ -1,4 +0,0 @@
-from transformers import CLIPProcessor, CLIPModel
-
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
