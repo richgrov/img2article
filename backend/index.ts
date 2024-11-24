a83c0011a6a81b0083994ffdfd219a761b6b8e33
timmyjinks <tysonjjenkins.github@gmail.com>
2024-11-24 16:08:18 -0700

    Remove backend using bun

--- backend/index.ts
+++
@@ -1,9 +0,0 @@
-const server = Bun.serve({
-  port: 8888,
-  hostname: "0.0.0.0",
-  async fetch(req) {
-    return new Response("200 Okay!");
-  },
-});
-
-console.log(`Listening on ${server.url}`);
