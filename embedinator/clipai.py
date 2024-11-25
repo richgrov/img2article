b410dc705c1de914e883470571dd14d37bf9fe9f
MasterDash5 <constant4337@molecularmail.com>
2024-11-25 14:15:21 -0700

    Encoding now works

--- embedinator/clipai.py
+++ embedinator/clipai.py
@@ -1,7 +1,15 @@
 import clip
 import torch
+from PIL import Image
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
-clip.tokenize("one two three")
+# image = preprocess(Image.open("CLIP.png")).unsqueeze(0).to(device)
+text = clip.tokenize("one two three").to(device)
+
+with torch.no_grad():
+    # image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+
+    print(text_features)
