78955980c4c289bcd7d6126502598e5aba0b3a72
timmyjinks <tysonjjenkins.github@gmail.com>
2024-11-27 14:32:55 -0700

    oops

--- backendinator/app.py
+++ backendinator/app.py
@@ -13,7 +13,7 @@ def image_encoding():
     data = request.files["file-upload"].stream.read()
     image = Image.open(BytesIO(data))
 
-    print(AIinator.embed_image(image))
+    image_embedding = AIinator.embed_image(image)
 
     return "200 YG2G"
 
