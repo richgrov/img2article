fa174d5487fb9b35f2438cbd762e390584e83dde
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-12-02 14:47:21 -0700

    Incorporate dark background into styling

--- imgpage/src/app/page.tsx
+++ imgpage/src/app/page.tsx
@@ -21,17 +21,21 @@ export default function Home() {
   }
 
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[url('/img/ai.jpg')] bg-cover bg-center bg-no-repeat text-white">
-      <div className="absolute inset-0 bg-black bg-opacity-70 z-10"></div>
-
+    <div
+      className="relative flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-cover bg-center bg-no-repeat text-white"
+      style={{
+        backgroundImage:
+          "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('/img/ai.jpg')",
+      }}
+    >
       <Image
         src={tysonCloud}
-        className="absolute top-4 right-4 z-20 h-24 w-auto"
+        className="absolute top-4 right-4 h-24 w-auto"
         alt="Logo"
       />
 
       <main
-        className={`relative flex flex-col items-center w-full max-w-2xl space-y-10 z-30 transition-transform duration-1000 ${
+        className={`relative flex flex-col items-center w-full max-w-2xl space-y-10 transition-transform duration-1000 ${
           animateUp ? "-translate-y-20" : ""
         }`}
       >
@@ -69,7 +73,7 @@ export default function Home() {
       <div id="data-box">
         {dataRecive && (
           <div>
-            <p className="text-white-500 bg-gray-800 px-4 py-2 rounded-md shadow-md font-semibold text-center z-30">
+            <p className="text-white-500 bg-gray-800 px-4 py-2 rounded-md shadow-md font-semibold text-center">
               {dataRecive}
             </p>
           </div>
