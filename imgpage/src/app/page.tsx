6444c2efff32a5679b5f0e6ce720fe9ddb59f829
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-12-02 18:35:54 -0700

    Frontend improvements (#13)
    * Fix usage of <img />
    
    * Remove old comments
    
    * Extract file upload to another component
    
    * Refactor
    
    * Simplify translate up animation
    
    * Move FileUpload to its own file
    
    * Incorporate dark background into styling

--- imgpage/src/app/page.tsx
+++ imgpage/src/app/page.tsx
@@ -1,90 +1,45 @@
 "use client";
 import React, { useState } from "react";
 import Typewriter from "typewriter-effect";
+import tysonCloud from "@/tysoncloud.png";
+import Image from "next/image";
+import { FileUpload } from "@/components/FileUpload";
 
 export default function Home() {
-  const [inputVisible, setInputVisible] = useState(false);
-  const [inputVisibleBack, setInputVisibleBack] = useState(false);
+  const [showFailedDialog, setShowFailedDialog] = useState(false);
+  const [animateUp, setAnimateUp] = useState(false);
   const [dataRecive, setArticle] = useState("");
-  const [dataTransfrom, setTransfrom] = useState(false);
-  const [fileUpload, setFileUpload] = useState<File | null>(null);
 
-  async function sendImage(event: React.ChangeEvent<HTMLInputElement>) {
-    const input = document.getElementById("file-upload") as HTMLInputElement;
-
-    if (input && input.files && input.files[0]) {
-      setArticle("Loading...")
-      noFile(false);
-      const formData = new FormData();
-      formData.append("file", input.files[0]);
-      setFileUpload(input.files[0]);
-
-      try {
-        const response = await fetch("http://localhost:8888/", {
-          method: "POST",
-          body: formData,
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-          transfrom();
-          article(data);
-        } else {
-          noWork();
-        }
-      } catch (error) {
-        console.error("Error uploading file:", error);
-        setArticle("");
-        noWork();
-      }
-    } else {
-      setArticle("");
-      noFile(true);
+  function onResponse(result: any) {
+    if (!result) {
+      setShowFailedDialog(true);
+      return;
     }
-  }
 
-  function noFile(check: boolean) {
-    setInputVisible(check);
-  }
-
-  function noWork() {
-    setInputVisibleBack(true);
-  }
-
-  function article(data: JSON) {
-    setArticle("data!");
-  }
-
-  function transfrom() {
-    setTransfrom(true);
+    setAnimateUp(true);
+    setArticle(JSON.stringify(result));
   }
 
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[url('/img/ai.jpg')] bg-cover bg-center bg-no-repeat text-white">
-      <div className="absolute inset-0 bg-black bg-opacity-70 z-10"></div>
-
-      <img
-        src="/img/tysoncloud.png"
-        className="absolute top-4 right-4 z-20 h-24"
+    <div
+      className="relative flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-cover bg-center bg-no-repeat text-white"
+      style={{
+        backgroundImage:
+          "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('/img/ai.jpg')",
+      }}
+    >
+      <Image
+        src={tysonCloud}
+        className="absolute top-4 right-4 h-24 w-auto"
         alt="Logo"
       />
 
-      {/* <div className="text-center text-white text-4xl absolute top-4 z-30">
-        <Typewriter
-          options={{
-            strings: [
-              "Search <br> Tool <br> Using <br> Picture <br> Embedded <br> Data",
-              "S.T.U.P.E.D",
-            ],
-            autoStart: true,
-            loop: true,
-            delay: 50,
-          }}
-        />
-      </div> */}
-
-      <main className="relative flex flex-col items-center w-full max-w-2xl space-y-10 z-30">
-        <div className={` text-center text-white text-4xl space-y-2 transition-transform duration-1000 ease-in-out ${dataTransfrom ? "-translate-y-20" : ""}`}>
+      <main
+        className={`relative flex flex-col items-center w-full max-w-2xl space-y-10 transition-transform duration-1000 ${
+          animateUp ? "-translate-y-20" : ""
+        }`}
+      >
+        <div className="text-center text-white text-4xl space-y-2 ease-in-out">
           <Typewriter
             options={{
               strings: [
@@ -99,83 +54,31 @@ export default function Home() {
           />
         </div>
 
-        <div
-          id="info-box"
-          className={`transition-transform duration-1000 ease-in-out ${
-            dataTransfrom ? "-translate-y-20" : ""
-          }`}
-        >
+        <div id="info-box">
           <h1 className="text-center text-3xl font-semibold text-white">
             Insert an Image
           </h1>
+        </div>
 
-          <div className="flex flex-col items-center space-y-8">
-            <div className="flex justify-center items-center space-y-4">
-              <label
-                htmlFor="file-upload"
-                className="cursor-pointer bg-gradient-to-r from-gray-800 to-gray-600 text-white py-3 px-8 rounded-lg shadow-lg hover:bg-gradient-to-l transition duration-300 ease-in-out text-lg font-semibold"
-              >
-                Upload Image
-              </label>
-              <input
-                id="file-upload"
-                type="file"
-                className="hidden"
-                accept=".jpg, .jpeg, .png"
-                onChange={sendImage}
-              />
-            </div>
+        <FileUpload onResponse={onResponse} />
 
-            <div className="flex justify-between items-center gap-6 bg-gray-700 p-4 rounded-lg shadow-md">
-              <div className="text-gray-300 font-medium text-sm">
-                Accepted file types:{" "}
-                <span className="font-semibold text-indigo-400">
-                  .jpg, .jpeg, .png
-                </span>
-              <div className="items-center text-center py-3 px-5 rounded-lg shadow-lg bg-gray-800">{fileUpload ? fileUpload.name : ""}</div> 
-              {/* keep maybe not */}
-              </div>
-              {/* <button
-                onClick={sendImage}
-                className="bg-indigo-600 text-white hover:bg-indigo-700 rounded-md py-2 px-6 shadow-md transition duration-300 ease-in-out transform hover:scale-105"
-              >
-                Enter
-              </button> */}
-            </div>
-
-            {inputVisible && (
-              <div>
-                <p className="text-red-500 bg-gray-800 px-4 py-2 rounded-md shadow-md font-semibold text-center">
-                  Please select a file to upload
-                </p>
-              </div>
-            )}
-
-            {inputVisibleBack && (
-              <div>
-                <p className="text-red-500 bg-gray-800 px-4 py-2 rounded-md shadow-md font-semibold text-center">
-                  Failed to send image
-                </p>
-              </div>
-            )}
+        {showFailedDialog && (
+          <div>
+            <p className="text-red-500 bg-gray-800 px-4 py-2 rounded-md shadow-md font-semibold text-center">
+              Failed to send image
+            </p>
           </div>
-        </div>
-
-        <div
-          id="data-box"
-          className={`transition-transform duration-1000 ease-in-out ${
-            dataTransfrom ? "-translate-y-20" : ""
-          }`}
-        >
-          {dataRecive && (
-            <div>
-              <p className="text-white-500 bg-gray-800 px-4 py-2 rounded-md shadow-md font-semibold text-center">
-                {dataRecive}
-              </p>
-            </div>
-          )}
-        </div>
+        )}
       </main>
+      <div id="data-box">
+        {dataRecive && (
+          <div>
+            <p className="text-white-500 bg-gray-800 px-4 py-2 rounded-md shadow-md font-semibold text-center">
+              {dataRecive}
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
