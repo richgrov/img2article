e6d4b19bdfeaa2cb368a3f52bb9ab10f99d2b4bf
timmyjinks <tysonjjenkins.github@gmail.com>
2024-12-03 11:19:58 -0700

    Add querying and respond with data

--- backendinator/app.py
+++ backendinator/app.py
@@ -1,10 +1,14 @@
 from io import BytesIO
 
-import AIinator
-from flask import Flask, make_response, request
+from flask import Flask, jsonify, make_response, request
+from milvusdal import MilvusDAL
 from PIL import Image
 
+import AIinator
+
 app = Flask(__name__)
+dal = MilvusDAL()
+dal.load_collection()
 
 
 @app.route("/", methods=["POST"])
@@ -15,12 +19,21 @@ def image_encoding():
     image = Image.open(BytesIO(data))
     image_embedding = AIinator.embed_image(image)
 
+    articles = dal.query(image_embedding)
+
+    array = []
+    for article in articles[0]:
+        title = str(article["entity"]["title"]).strip()
+        link = "https://wikipedia.org/wiki/" + title.replace(" ", "_").strip()
+        array.append({"title": title, "link": link})
+        print(article["entity"]["title"], "\n")
+
     response = make_response("200 YG2G")
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type"
 
-    return response
+    return jsonify(array)
 
 
 if __name__ == "__main__":
